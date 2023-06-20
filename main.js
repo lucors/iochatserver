dafe4cceb0c0ecdf2d44266262ba48393d303ba7
lucors <thesourcecode.max@yandex.ru>
2023-06-20 22:14:16 +0400

    Изменен принцип инициализации сервера

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const handler = require('./handler.js');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const ioPort = 9000;
-const io = require('socket.io')();
+
+const httpServer = createServer();
+const io = new Server(httpServer);
 
 handler.prepare();
 io.on('connection', handler.onConnect);
-io.listen(ioPort);
+httpServer.listen(ioPort);
 console.log(`Сервер запущен на порту: ${ioPort}`);
+
